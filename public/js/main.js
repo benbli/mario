d0a0b64debff74c2bf132b272dafd7589c59094a
Ben Li <benbli@gmail.com>
2018-11-28 15:24:05 -0800

    init

---
+++ public/js/main.js
@@ -0,0 +1,38 @@
+import Timer from './Timer.js'
+import { loadLevel } from './loaders.js'
+import { createMario } from './entities.js'
+import { createCollisionLayer } from './layers.js'
+import { setupKeyboard } from './input.js'
+
+const canvas = document.getElementById('canvas')
+const context = canvas.getContext('2d')
+
+Promise.all([createMario(), loadLevel('1-1')]).then(([mario, level]) => {
+  mario.pos.set(64, 64)
+
+  level.entities.add(mario)
+  // level.comp.layers.push(createCollisionLayer(level))
+
+  const input = setupKeyboard(mario)
+
+  input.listenTo(window)
+
+  // draggable mario
+  // ;[('mousedown', 'mousemove')].forEach(eventName => {
+  //   canvas.addEventListener(eventName, event => {
+  //     if (event.buttons === 1) {
+  //       mario.vel.set(0, 0)
+  //       mario.pos.set(event.offsetX, event.offsetY)
+  //     }
+  //   })
+  // })
+
+  const timer = new Timer(1 / 60)
+
+  timer.update = function update(deltaTime) {
+    level.update(deltaTime)
+    level.comp.draw(context)
+  }
+
+  timer.start()
+})
